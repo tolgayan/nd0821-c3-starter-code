d943cdceafab488fdbccc637692927842d06e2ea
Tolga Ayan <tolgayan98@gmail.com>
2021-12-26 15:00:58 +0300

    fix heroku dvc command

--- starter/main.py
+++ starter/main.py
@@ -1,19 +1,20 @@
 # Put the code for your API here.
 
-from fastapi import FastAPI
-from starter.starter.basemodel import Data
-from starter.starter.infer import infer
 import os
-
-
-app = FastAPI()
-
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
     if os.system("dvc pull") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
+
+
+from fastapi import FastAPI
+from starter.starter.basemodel import Data
+from starter.starter.infer import infer
+
+
+app = FastAPI()
 
 
 @app.get("/")
